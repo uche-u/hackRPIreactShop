f9a676205bfc2de2202813765e250a56a56947b9
uche-u <70341288+uche-u@users.noreply.github.com>
2024-02-24 14:11:31 -0500

    setting up first react website!

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import styles from "./page.module.css";
+import ComponentName from "@/components/compOne/compOne";
+
+export default function Page() {
+return(<div>
+  <ComponentName />
+</div>);
+}
---
+++ components/compOne/compOne.tsx
@@ -0,0 +1,6 @@
+export default function compOne() {
+    return (
+     <h1>This is my new Component</h1>
+    );
+  }
+  
